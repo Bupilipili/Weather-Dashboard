5ec79cd590500f9ef02d42a1b6a0c8c440042cb5
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-11-14 13:20:04 +0300

    Create weatherSlice.js

---
+++ src/redux/weatherSlice.js
@@ -0,0 +1,63 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Define the initial state
+const initialState = {
+  filter: '',
+  cities: [], // Array to store city data
+  selectedCity: null, // Store the selected city's weather data
+  loading: false,
+  error: null,
+};
+
+// Define your API base URL
+const API_BASE_URL = 'https://api.weatherapi.com/v1';
+
+// Create an async thunk to fetch city weather data
+export const fetchCityWeatherAsync = createAsyncThunk(
+  'weather/fetchCityWeather',
+  async (cityName) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/current.json?key=786177d666c24f5f86d121735230609&q=${cityName}`);
+      return response.data;
+    } catch (error) {
+      throw error.response.data.error.message;
+    }
+  },
+);
+
+// Create a slice for weather data
+const weatherSlice = createSlice({
+  name: 'weather',
+  initialState,
+  reducers: {
+    setFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+    selectCity: (state, action) => {
+      state.selectedCity = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCityWeatherAsync.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchCityWeatherAsync.fulfilled, (state, action) => {
+        state.loading = false;
+        state.selectedCity = action.payload;
+        state.cities = [...state.cities, action.payload.location.name];
+      })
+      .addCase(fetchCityWeatherAsync.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+// Export action creators
+export const { setFilter, selectCity } = weatherSlice.actions;
+
+// Export the reducer
+export default weatherSlice.reducer;
