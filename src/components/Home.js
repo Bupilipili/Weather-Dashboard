1459050af4c8cbc865f8e0af1c257e930a53250b
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-11-16 07:25:10 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios'; // Import axios for making HTTP requests
 import fetchWeatherByCityName from '../services/api';
 import './Home.css';
+
+// Function to fetch the city name based on latitude and longitude using OpenCage Geocoding API
+const fetchCityNameByCoordinates = async (latitude, longitude) => {
+  const apiKey = '4a0a7c3f2096409eb53cc6d27fc91d36'; // Replace with your actual API key
+  const apiUrl = `https://api.opencagedata.com/geocode/v1/json?key=${apiKey}&q=${latitude}+${longitude}&pretty=1`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const city = response.data.results[0]?.components.city;
+
+    if (city) {
+      return city;
+    } else {
+      console.error('City not found in reverse geocoding response');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching city name:', error.message);
+    return null;
+  }
+};
 
 function Home() {
   const [cities, setCities] = useState(['Dar es Salaam', 'Tokyo', 'Lindi', 'London', 'Mtwara', 'Mwanza', 'Dodoma', 'Mbeya', 'Paris']);
   const [filteredCities, setFilteredCities] = useState(cities);
   const [selectedCity, setSelectedCity] = useState('');
-  const [weatherData, setWeatherData] = useState({}); // Initialize as an empty object
+  const [weatherData, setWeatherData] = useState({});
+  const [userLocation, setUserLocation] = useState(null);
+  const [searchHistory, setSearchHistory] = useState([]);
+  const [showHistory, setShowHistory] = useState(false);
 
   useEffect(() => {
     // Load weather data for all cities in the array
@@ -21,7 +46,21 @@ function Home() {
         console.error(error);
       }
     });
-  }, [cities]);
+
+    // Check for geolocation support
+    if (navigator.geolocation && !userLocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position.coords;
+          const city = await fetchCityNameByCoordinates(latitude, longitude);
+          setUserLocation(city);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }, [userLocation]);
 
   const handleCityChange = async (e) => {
     const { value } = e.target;
@@ -38,6 +77,12 @@ function Home() {
         if (data && data.current) {
           // Data is available, update weatherData with the new city data
           setWeatherData((prevData) => ({ ...prevData, [value]: data }));
+
+          // Add the current input to the search history
+          setSearchHistory((prevHistory) => [...prevHistory, value]);
+
+          // Save search history to local storage
+          localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 
           // Add the current input to the array list
           setCities((prevCities) => [...prevCities, value]);
@@ -67,36 +112,38 @@ function Home() {
       }
     } else {
       setSelectedCity(value);
-
-      // Check if the input matches any existing city from the API data
-      const matchingCity = Object.keys(weatherData).find(
-        (city) => city.toLowerCase() === value.toLowerCase(),
-      );
-
-      // If the input matches an existing city, set filteredCities to contain only that matching city
-      if (matchingCity) {
-        setFilteredCities([matchingCity]);
-      } else {
-        // Filter the cities based on the input value
-        const filtered = cities.filter((city) => city.toLowerCase().includes(value.toLowerCase()));
-
-        // Update filteredCities with the filtered list
-        setFilteredCities(filtered);
-      }
     }
   };
 
   return (
     <div className="container">
-      <input
-        className='search'
-        type="text"
-        placeholder="Search for city"
-        value={selectedCity}
-        onChange={handleCityChange}
-        onKeyPress={handleCityChange}
-      />
+           <div className="search-container">
+        <input
+          className='search'
+          type="text"
+          placeholder="Search for city"
+          value={selectedCity}
+          onChange={handleCityChange}
+          onKeyPress={handleCityChange}
+          onFocus={() => setShowHistory(true)}
+          onBlur={() => setShowHistory(false)}
+        />
+        {showHistory && searchHistory.length > 0 && (
+          <div className="search-history">
+            <ul className='histo'>
+              {searchHistory.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
       <div id='error-message'></div>
+      {userLocation ? (
+        <p> Current location, {userLocation}.</p>
+      ) : (
+        <div className="loading-spinner" />
+      )}
       {document.getElementById('error-message') && document.getElementById('error-message').textContent === '' && (
         <ul className="card-list">
           {filteredCities.map((city) => (
